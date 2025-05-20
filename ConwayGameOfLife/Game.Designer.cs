62d07f264916d621a92e51e71dc4d697b1e864aa
PetrF <argruss@gmail.com>
2025-05-20 19:05:25 +0200

    Improve painting

--- ConwayGameOfLife/Game.Designer.cs
+++ ConwayGameOfLife/Game.Designer.cs
@@ -26,83 +26,89 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.Game_Arena = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.Play_Button = new System.Windows.Forms.Button();
-            this.Close_Button = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.Game_Arena)).BeginInit();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
+        private void InitializeComponent() {
+            components = new System.ComponentModel.Container();
+            Game_Arena = new System.Windows.Forms.PictureBox();
+            panel1 = new System.Windows.Forms.Panel();
+            Play_Button = new System.Windows.Forms.Button();
+            Close_Button = new System.Windows.Forms.Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)Game_Arena).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
             // 
             // Game_Arena
             // 
-            this.Game_Arena.BackColor = System.Drawing.SystemColors.Control;
-            this.Game_Arena.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Game_Arena.Location = new System.Drawing.Point(0, 38);
-            this.Game_Arena.Name = "Game_Arena";
-            this.Game_Arena.Size = new System.Drawing.Size(250, 212);
-            this.Game_Arena.TabIndex = 0;
-            this.Game_Arena.TabStop = false;
-            this.Game_Arena.Paint += new System.Windows.Forms.PaintEventHandler(this.Game_Arena_Paint);
-            this.Game_Arena.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Game_Arena_MouseClick);
+            Game_Arena.BackColor = System.Drawing.SystemColors.Control;
+            Game_Arena.Dock = System.Windows.Forms.DockStyle.Fill;
+            Game_Arena.Location = new System.Drawing.Point(0, 48);
+            Game_Arena.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Game_Arena.Name = "Game_Arena";
+            Game_Arena.Size = new System.Drawing.Size(250, 264);
+            Game_Arena.TabIndex = 0;
+            Game_Arena.TabStop = false;
+            Game_Arena.Paint += Game_Arena_Paint;
+            Game_Arena.MouseDown += Game_Arena_MouseDown;
+            Game_Arena.MouseMove += Game_Arena_MouseMove;
+            Game_Arena.MouseUp += Game_Arena_MouseUp;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
-            this.panel1.Controls.Add(this.Play_Button);
-            this.panel1.Controls.Add(this.Close_Button);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 38);
-            this.panel1.TabIndex = 1;
-            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
-            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
-            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            panel1.Controls.Add(Play_Button);
+            panel1.Controls.Add(Close_Button);
+            panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            panel1.Location = new System.Drawing.Point(0, 0);
+            panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(250, 48);
+            panel1.TabIndex = 1;
+            panel1.MouseDown += panel1_MouseDown;
+            panel1.MouseMove += panel1_MouseMove;
+            panel1.MouseUp += panel1_MouseUp;
             // 
             // Play_Button
             // 
-            this.Play_Button.Location = new System.Drawing.Point(168, 0);
-            this.Play_Button.Name = "Play_Button";
-            this.Play_Button.Size = new System.Drawing.Size(38, 38);
-            this.Play_Button.TabIndex = 3;
-            this.Play_Button.Text = "P";
-            this.Play_Button.UseVisualStyleBackColor = true;
-            this.Play_Button.Click += new System.EventHandler(this.play_button_Click);
+            Play_Button.Location = new System.Drawing.Point(168, 0);
+            Play_Button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Play_Button.Name = "Play_Button";
+            Play_Button.Size = new System.Drawing.Size(38, 48);
+            Play_Button.TabIndex = 3;
+            Play_Button.Text = "P";
+            Play_Button.UseVisualStyleBackColor = true;
+            Play_Button.Click += play_button_Click;
             // 
             // Close_Button
             // 
-            this.Close_Button.Location = new System.Drawing.Point(212, 0);
-            this.Close_Button.Name = "Close_Button";
-            this.Close_Button.Size = new System.Drawing.Size(38, 38);
-            this.Close_Button.TabIndex = 2;
-            this.Close_Button.Text = "X";
-            this.Close_Button.UseVisualStyleBackColor = true;
-            this.Close_Button.Click += new System.EventHandler(this.Close_Button_Click);
+            Close_Button.Location = new System.Drawing.Point(212, 0);
+            Close_Button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Close_Button.Name = "Close_Button";
+            Close_Button.Size = new System.Drawing.Size(38, 48);
+            Close_Button.TabIndex = 2;
+            Close_Button.Text = "X";
+            Close_Button.UseVisualStyleBackColor = true;
+            Close_Button.Click += Close_Button_Click;
             // 
             // timer1
             // 
-            this.timer1.Interval = 200;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            timer1.Interval = 1;
+            timer1.Tick += timer1_Tick;
             // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(250, 250);
-            this.Controls.Add(this.Game_Arena);
-            this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Game";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Game";
-            ((System.ComponentModel.ISupportInitialize)(this.Game_Arena)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.ResumeLayout(false);
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(250, 312);
+            Controls.Add(Game_Arena);
+            Controls.Add(panel1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Name = "Game";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Game";
+            ((System.ComponentModel.ISupportInitialize)Game_Arena).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
 
         }
 
--- ConwayGameOfLife/Game.cs
+++ ConwayGameOfLife/Game.cs
@@ -4,18 +4,16 @@ using System.Windows.Forms;
 
 namespace ConwayGameOfLife
 {
-    public partial class Game : Form
-    {
+    public partial class Game : Form {
         Arena arena;
-        public Game(int x, int y)
-        { 
-            arena = new Arena(x/10,y/10);
+        public Game(int x, int y) {
+            arena = new Arena(x / 10, y / 10);
             #region Making Form look right
             InitializeComponent();
             Size = new Size(x, y + panel1.Height);
             //I didn't find better way
             Close_Button.Size = new Size(panel1.Height, panel1.Height);
-            Close_Button.Location = new Point(panel1.Width-panel1.Height, 0);
+            Close_Button.Location = new Point(panel1.Width - panel1.Height, 0);
             Play_Button.Size = new Size(panel1.Height, panel1.Height);
             Play_Button.Location = new Point(panel1.Width - panel1.Height * 2 - 10, 0);
             #endregion
@@ -25,32 +23,25 @@ namespace ConwayGameOfLife
         /// <summary>
         /// Represents game grid and works with that
         /// </summary>
-        enum Cells {white = 0, red, blue, red_dead, blue_dead}
-        class Arena
-        {
+        enum Cells { white = 0, red, blue, red_dead, blue_dead }
+        class Arena {
             public Cells[,] status { get; private set; }
 
-            public Arena(int x, int y)
-            {
+            public Arena(int x, int y) {
                 status = new Cells[x, y];
             }
 
             /// <summary>
             /// rewrites status according to game rules
             /// </summary>
-            public void newGeneration()
-            {
+            public void newGeneration() {
                 Cells[,] temp = (Cells[,])status.Clone();
-                for (int i = 0; i < status.GetLength(0); i++)
-                {
-                    for(int j = 0; j < status.GetLength(1); j++)
-                    {
-                        if (IsWhite(i,j))
-                        {
+                for (int i = 0; i < status.GetLength(0); i++) {
+                    for (int j = 0; j < status.GetLength(1); j++) {
+                        if (IsWhite(i, j)) {
                             int red = neighbours(Cells.red, i, j);
                             int blue = neighbours(Cells.blue, i, j);
-                            if (red != blue)
-                            {
+                            if (red != blue) {
                                 if (red == 3)
                                     temp[i, j] = Cells.red;
                                 else if (blue == 3)
@@ -59,18 +50,16 @@ namespace ConwayGameOfLife
                         }
                         else //colored space
                         {
-                            if (neighbours(status[i, j], i, j) < 3 || neighbours(status[i, j], i, j) > 4)
-                            {
-                                if (status[i,j] == Cells.red) {
-                                    temp[i,j] = Cells.red_dead;
+                            if (neighbours(status[i, j], i, j) < 3 || neighbours(status[i, j], i, j) > 4) {
+                                if (status[i, j] == Cells.red) {
+                                    temp[i, j] = Cells.red_dead;
                                 }
                                 else {
                                     temp[i, j] = Cells.blue_dead;
                                 }
                             }
-                            else
-                            {
-                                temp[i, j] = status[i,j];
+                            else {
+                                temp[i, j] = status[i, j];
                             }
                         }
                     }
@@ -78,8 +67,8 @@ namespace ConwayGameOfLife
                 status = temp;
             }
 
-            private bool IsWhite(int x, int y) {
-                return status[x,y] is Cells.white or Cells.red_dead or Cells.blue_dead;
+            public bool IsWhite(int x, int y) {
+                return status[x, y] is Cells.white or Cells.red_dead or Cells.blue_dead;
             }
 
             /// <summary>
@@ -89,18 +78,14 @@ namespace ConwayGameOfLife
             /// <param name="X">X coordinates</param>
             /// <param name="Y">Y coordinates</param>
             /// <returns></returns>
-            private int neighbours(Cells color, int X, int Y)
-            {
+            private int neighbours(Cells color, int X, int Y) {
                 int result = 0;
-                for (int i = X-1; i <= X+1; i++)
-                {
-                    for (int j = Y-1; j <= Y+1; j++)
-                    {
-                        if (status[(i + status.GetLength(0)) % status.GetLength(0),(j + status.GetLength(1)) % status.GetLength(1)] == color)
-                        {
+                for (int i = X - 1; i <= X + 1; i++) {
+                    for (int j = Y - 1; j <= Y + 1; j++) {
+                        if (status[(i + status.GetLength(0)) % status.GetLength(0), (j + status.GetLength(1)) % status.GetLength(1)] == color) {
                             result++;
                         }
-                        
+
                     }
                 }
                 return result;
@@ -108,41 +93,35 @@ namespace ConwayGameOfLife
         }
 
         private Pen pen = new Pen(Color.Black, 1);
-        private SolidBrush blueBrush = new SolidBrush(Color.Blue);
-        private SolidBrush deadbluBrush = new SolidBrush(Color.Aqua);
+        private SolidBrush blueBrush = new SolidBrush(Color.DarkBlue);
+        private SolidBrush deadbluBrush = new SolidBrush(Color.DeepSkyBlue);
         private SolidBrush whiteBrush = new SolidBrush(Color.White);
-        private SolidBrush redBrush = new SolidBrush(Color.Red);
+        private SolidBrush redBrush = new SolidBrush(Color.DarkRed);
         private SolidBrush deadredBrush = new SolidBrush(Color.IndianRed);
-        private void Game_Arena_Paint(object sender, PaintEventArgs e)
-        {
+        private void Game_Arena_Paint(object sender, PaintEventArgs e) {
             Graphics g = e.Graphics;
             //Create grid
             {
-                for (int i = 0; i <= Game_Arena.Width; i += 10) 
-                {
+                for (int i = 0; i <= Game_Arena.Width; i += 10) {
                     g.DrawLine(pen, i, 0, i, Game_Arena.Height);
                 }
-                for (int i = 0; i <= Game_Arena.Height; i += 10) 
-                {
+                for (int i = 0; i <= Game_Arena.Height; i += 10) {
                     g.DrawLine(pen, 0, i, Game_Arena.Width, i);
-                } 
+                }
             }
 
             //fill cells with right colors
-            for (int i = 0; i < arena.status.GetLength(0); i++)
-            {
-                for (int j = 0; j < arena.status.GetLength(1); j++)
-                {
-                    switch (arena.status[i, j])
-                    {
+            for (int i = 0; i < arena.status.GetLength(0); i++) {
+                for (int j = 0; j < arena.status.GetLength(1); j++) {
+                    switch (arena.status[i, j]) {
                         case Cells.blue:
                             g.FillRectangle(blueBrush, i * 10 + 1, j * 10 + 1, 9, 9);
                             break;
                         case Cells.white:
-                            g.FillRectangle(whiteBrush, i*10 + 1, j * 10 + 1, 9, 9);
+                            g.FillRectangle(whiteBrush, i * 10 + 1, j * 10 + 1, 9, 9);
                             break;
                         case Cells.red:
-                            g.FillRectangle(redBrush, i*10 + 1, j * 10 + 1, 9, 9);
+                            g.FillRectangle(redBrush, i * 10 + 1, j * 10 + 1, 9, 9);
                             break;
                         case Cells.red_dead:
                             g.FillRectangle(deadredBrush, i * 10 + 1, j * 10 + 1, 9, 9);
@@ -155,72 +134,106 @@ namespace ConwayGameOfLife
             }
         }
 
-        private void Close_Button_Click(object sender, EventArgs e)
-        {
+        private void Close_Button_Click(object sender, EventArgs e) {
             Launcher.Instance.Close();
         }
 
         #region Dragging
         private bool dragging = false;
         private Point CurPos;
-        private void panel1_MouseDown(object sender, MouseEventArgs e)
-        {
+        private void panel1_MouseDown(object sender, MouseEventArgs e) {
             dragging = true;
             CurPos = Cursor.Position;
         }
 
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (dragging)
-            {
+        private void panel1_MouseMove(object sender, MouseEventArgs e) {
+            if (dragging) {
                 Point dif = Point.Subtract(Cursor.Position, new Size(CurPos));
                 CurPos = Cursor.Position;
                 Location = Point.Add(Location, new Size(dif));
             }
         }
 
-        private void panel1_MouseUp(object sender, MouseEventArgs e)
-        {
+        private void panel1_MouseUp(object sender, MouseEventArgs e) {
             dragging = false;
         }
         #endregion
 
         #region Setting up game arena
-        private void Game_Arena_MouseClick(object sender, MouseEventArgs e)
-        {
-            (int X, int Y) = (e.Location.X/10,e.Location.Y/10);
-            if (e.Button == MouseButtons.Left)
-            {
-                arena.status[X, Y] = Cells.red;
+        private void Game_Arena_MouseClick(object sender, MouseEventArgs e) {
+            //(int X, int Y) = (e.Location.X / 10, e.Location.Y / 10);
+            //if (e.Button == MouseButtons.Left) {
+            //    if (arena.status[X, Y] == Cells.white)
+            //        arena.status[X, Y] = Cells.red;
+            //    else arena.status[X, Y] = Cells.white;
+            //}
+            //else {
+            //    if (arena.status[X, Y] == Cells.white)
+            //        arena.status[X, Y] = Cells.blue;
+            //    else arena.status[X, Y] = Cells.white;
+            //}
+            //Game_Arena.Refresh();
+        }
+
+        private bool painting = false;
+        private Cells from = Cells.white, to = Cells.white;
+        private void Game_Arena_MouseDown(object sender, MouseEventArgs e) {
+            (int X, int Y) = (e.Location.X / 10, e.Location.Y / 10);
+            painting = true;
+            if (e.Button == MouseButtons.Left) {
+                if (arena.status[X, Y] == Cells.red) {
+                    from = Cells.red; to = Cells.white;
+                }
+                else { 
+                    from = Cells.white; to = Cells.red;
+                }
             }
-            else
-            {
-                arena.status[X, Y] = Cells.blue;
+            else {
+                if (arena.status[X, Y] == Cells.blue) {
+                    from = Cells.blue; to = Cells.white;
+                }
+                else {
+                    from = Cells.white; to = Cells.blue;
+                }
             }
+            arena.status[X, Y] = to;
             Game_Arena.Refresh();
         }
+
+        private void Game_Arena_MouseUp(object sender, MouseEventArgs e) {
+            painting = false;
+        }
+
+        private void Game_Arena_MouseMove(object sender, MouseEventArgs e) {
+            if (painting) {
+                (int X, int Y) = (e.Location.X / 10, e.Location.Y / 10);
+                if (arena.status[X, Y] == from) {
+                    arena.status[X, Y] = to;
+                }
+                Game_Arena.Refresh();
+            }
+        }
+
         #endregion
 
         #region Starting simulation
-        private void play_button_Click(object sender, EventArgs e)
-        {
-            if (Play_Button.Text == "P")
-            {
+        private void play_button_Click(object sender, EventArgs e) {
+            if (Play_Button.Text == "P") {
                 Play_Button.Text = "L";
                 timer1.Enabled = true;
             }
-            else
-            {
+            else {
                 Play_Button.Text = "P";
                 timer1.Enabled = false;
             }
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
+        private void timer1_Tick(object sender, EventArgs e) {
             arena.newGeneration();
             Game_Arena.Refresh();
         }
         #endregion
+
+
     }
 }
